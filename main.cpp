92869c21df1f922abb22333d0eba23193af2834a
vrinda0115 <120307639+vrinda0115@users.noreply.github.com>
2023-01-28 22:44:59 +0530

    matrix validate

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@
 
 using namespace std;
 
-int matrix[3][3]={0};
-
+int matrix[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+char turn = 'x' ;
+int choice;
 int player1, player2;
-bool 
-int matrixValidate();
+bool  matrixValidate();
 int playerSelection();
 void printmatrix();
 int playerinput();
@@ -19,9 +19,12 @@ int gameLoop();
 int main(){
     printmatrix();
     playerSelection();
+
+    return 0;
 }
 
 void printmatrix(){
+    cout<<"Matrix shown below: "
     for(int i=0; i<3; i++)
     {
         for(int j=0; j<3; j++)
@@ -33,38 +36,47 @@ void printmatrix(){
     }
 }
 
-/*int matrixValidate()
-{
-    if (matrix[0][j] && matrix[1][j] && matrix[2][j] == 1)
+ matrixValidate()
+{     
+    bool winner= false;
+    if (matrix[0][j] && matrix[1][j] && matrix[2][j] == 1 )
     {
         cout<<("Player 1 wins ");
+        winner = true;
     }
     else if (matrix[0][j] && matrix[1][j] && matrix[2][j] == 2)
     {
         cout<<("Player 2 wins ");
+        winner = true;
+
     }
-    else if (matrix[0][0] && matrix[1][1] && matrix[2][2] == 1)
+    else if (matrix[0][0] && matrix[1][1] && matrix[2][2] == 1 && turn=='x')
     {
         cout<<("Player 1 wins ");
+        winner = true;
     }
-    else if (matrix[0][0] && matrix[1][1] && matrix[2][2] == 2)
+    else if (matrix[0][0] && matrix[1][1] && matrix[2][2] == 2 && ) 
     {
         cout<<("Player 2 wins ");
+        winner = true;
     }
-    else if (matrix[0][2] && matrix[1][1] && matrix[2][0] == 1)
+    else if (matrix[0][2] && matrix[1][1] && matrix[2][0] == 1 && )
     {
         cout<<("Player 1 wins ");
+        winner = true;
     }
-    else if (matrix[0][2] && matrix[1][1] && matrix[2][0] == 2)
+    else if (matrix[0][2] && matrix[1][1] && matrix[2][0] == 2 && )
     {
         cout<<("Player 2 wins ");
+        winner = true;
     }
     else
     {
         cout<<("its a TIE!");
     }
+    return winner;
 
-}*/
+}
 
 int playerSelection()
 {
@@ -83,6 +95,81 @@ int playerSelection()
     else
     {
         cout<<"Ivalid input";
+    }
+
+}
+int playerinput()
+{
+    if(turn=='x')
+    {
+        cout<<"Player enter input : ";
+    }
+    else if (turn== 'o')
+    {
+        cout<<"Player enter input : ";
+    }
+    cin>>choice;
+
+    switch(choice){
+
+    case 1 :
+            board[0][0] ; 
+            break;
+    case 2 : board[0][1] ; 
+            break;
+    case 3 : board[0][2] ; 
+            break;
+    case 4 : board[1][0] ; 
+            break;
+    case 5 : board[1][1] ; 
+            break;
+    case 6 : board[1][2] ; 
+            break;
+    case 7 : board[2][0] ; 
+            break;
+    case 8 : board[2][1] ; 
+            break;
+    case 9 : board[2][2] ; 
+            break;
+    default: cout<<"Invalid move";
+    }
+}
+
+
+
+
+
+
+
+
+int gamemenu()
+{
+    cout<<"Enter choice y or n: ";
+    cin>>input;
+
+    switch(input) 
+    {
+        case 1 : 
+        if(input==y)
+        {
+            printmatrix();
+        }
+        else
+        {
+            cout<<"Invalid input";
+        }
+        case 2 : 
+        if(input==n)
+        {
+            cout<<"Goodbye";
+
+        }
+        else
+        {
+            
+        }
+        
+
     }
 
 }
